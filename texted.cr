0d3d434b804fa2a738d58d28b4c0cf06f2dfafe6
Alexey Yurchenko <homonoidian@yandex.ru>
2025-03-24 01:50:48 +0300

    soma: input progress: Implement basic drawing of text input
    Text input still has a lot of bugs/unhandled corner cases. Will need to
    think about them.

--- texted.cr
+++ texted.cr
@@ -1,3 +1,11 @@
+# missing features:
+#  - small stuff (missing keybindings, cannot switch sides in Shift, etc.)
+#  - single-line appearance
+# <----> here we will be able to start using the thing for command palette
+#  - multiline appearance with scrolling (up/down, left/right, mouse wheel, Shift + mouse wheel)
+#  - multiline with wrapping
+#  - point and click
+
 require "./src/wirewright"
 
 base = File.read("input.soma.wwml")
@@ -35,22 +43,155 @@ step = exhR(
   )
 )
 
-# src = <<-SRC
-# Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi
-# Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit
-# nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud
-# officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia
-# dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris
-# ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim.
-# Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.
-# SRC
+src = <<-SRC
+Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi
+Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit
+nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud
+officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia
+dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris
+ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim.
+Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.
+SRC
 
-model = Term.of(Term[], "", :|, "", Term[], Term[])
+def text_to_input(text)
+  lines = text.lines(chomp: true)
+  if lines.empty?
+    return Term.of(Term[], "", :|, "", Term[], Term[])
+  end
 
-def input(model : Term)
-  Term.of(:p, ML.display(model), style: "text-neutral-300 font-mono text-xs")
+  Term.of(Term[lines.to_readonly_slice[...-1]], lines[-1], :|, "", Term[], Term[])
+end
 
-  # Term.of(:group, l, Term.of({:self, :rect}, style: "w-px h-max"), r, style: "content text-neutral-300 flow-block")
+model = text_to_input(src)
+
+# FIXME: deduplicate (use Alloy template?)
+def input(model : Term, style = "font-mono text-neutral-300")
+  Term.case(model) do
+    matchpi %{[(above_string*) l_string | r_string (below_string*) (_*)]} do
+      Term::Dict.build do |commit|
+        commit << :group
+        commit.with(:style, "content flow-col")
+
+        above.items.each do |line|
+          commit << Term.of(:p, line, style: style)
+        end
+
+        commit << Term.of(:group,
+          Term.of(:p, l, style: style),
+          Term.of({:self, :rect}, style: "w-0 h-max bg-blue-500 ring-r ring-blue-500"),
+          Term.of(:p, r, style: style),
+          style: "content flow-row")
+
+        below.items.each do |line|
+          commit << Term.of(:p, line, style: style)
+        end
+      end
+    end
+
+    matchpi %{[(above_string*) l_string (<| selected_string) r_string (below_string*) (_*)]} do
+      Term::Dict.build do |commit|
+        commit << :group
+        commit.with(:style, "content flow-col")
+
+        above.items.each do |line|
+          commit << Term.of(:p, line, style: style)
+        end
+
+        commit << Term.of(:group,
+          Term.of(:p, l, style: style),
+          Term.of({:self, :rect}, style: "w-0 h-max bg-yellow-500 ring-l ring-yellow-500"),
+          Term.of(:p, selected, style: "#{style} text-white bg-blue-500"),
+          Term.of(:p, r, style: style),
+          style: "content flow-row")
+
+        below.items.each do |line|
+          commit << Term.of(:p, line, style: style)
+        end
+      end
+    end
+
+    matchpi %{[(above_string*) l_string (|> selected_string) r_string (below_string*) (_*)]} do
+      Term::Dict.build do |commit|
+        commit << :group
+        commit.with(:style, "content flow-col")
+
+        above.items.each do |line|
+          commit << Term.of(:p, line, style: style)
+        end
+
+        commit << Term.of(:group,
+          Term.of(:p, l, style: style),
+          Term.of(:p, selected, style: "#{style} text-white bg-blue-500"),
+          Term.of({:self, :rect}, style: "w-0 h-max bg-yellow-500 ring-r ring-yellow-500"),
+          Term.of(:p, r, style: style),
+          style: "content flow-row")
+
+        below.items.each do |line|
+          commit << Term.of(:p, line, style: style)
+        end
+      end
+    end
+
+    matchpi %{[(above_string*) l_string (<| front_string mid_string* back_string) r_string (below_string*) (_*)]} do
+      Term::Dict.build do |commit|
+        commit << :group
+        commit.with(:style, "content flow-col")
+
+        above.items.each do |line|
+          commit << Term.of(:p, line, style: style)
+        end
+
+        commit << Term.of(:group,
+          Term.of(:p, l, style: style),
+          Term.of({:self, :rect}, style: "w-0 h-max bg-yellow-500 ring-l ring-yellow-500"),
+          Term.of(:p, front, style: "#{style} text-white bg-blue-500"),
+          style: "content flow-row")
+
+        mid.items.each do |line|
+          commit << Term.of(:p, line, style: "#{style} text-white bg-blue-500")
+        end
+
+        commit << Term.of(:group,
+          Term.of(:p, back, style: "#{style} text-white bg-blue-500"),
+          Term.of(:p, r, style: style),
+          style: "content flow-row")
+
+        below.items.each do |line|
+          commit << Term.of(:p, line, style: style)
+        end
+      end
+    end
+
+    matchpi %{[(above_string*) l_string (|> front_string mid_string* back_string) r_string (below_string*) (_*)]} do
+      Term::Dict.build do |commit|
+        commit << :group
+        commit.with(:style, "content flow-col")
+
+        above.items.each do |line|
+          commit << Term.of(:p, line, style: style)
+        end
+
+        commit << Term.of(:group,
+          Term.of(:p, l, style: style),
+          Term.of(:p, front, style: "#{style} text-white bg-blue-500"),
+          style: "content flow-row")
+
+        mid.items.each do |line|
+          commit << Term.of(:p, line, style: "#{style} text-white bg-blue-500")
+        end
+
+        commit << Term.of(:group,
+          Term.of(:p, back, style: "#{style} text-white bg-blue-500"),
+          Term.of({:self, :rect}, style: "w-0 h-max bg-yellow-500 ring-r ring-yellow-500"),
+          Term.of(:p, r, style: style),
+          style: "content flow-row")
+
+        below.items.each do |line|
+          commit << Term.of(:p, line, style: style)
+        end
+      end
+    end
+  end
 end
 
 require "./uiRb"
