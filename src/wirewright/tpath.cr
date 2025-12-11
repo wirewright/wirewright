ecc4fc0aab0057d57e04407cc7449ec0993494a3
Alexey Yurchenko <homonoidian@yandex.ru>
2025-12-12 02:23:18 +0300

    ww: Add `Tpath.each(term : Term, &)`

--- src/wirewright/tpath.cr
+++ src/wirewright/tpath.cr
@@ -57,6 +57,44 @@ module Ww
       Value.new(Term.of(object))
     end
 
+    # Yields `Tpath`s and their corresponding values in *term* to the block.
+    def self.each(term : Term, & : Tpath, Term ->)
+      state_value = -2 # const
+      state_key = -1   # const
+      state_iter = 0   # and above
+
+      stack = [{state: state_value, path: Tpath[], term: term}]
+
+      while rec = stack.pop?
+        case rec[:state]
+        when state_key
+          unless rec[:term].type.dict?
+            yield rec[:path], rec[:term]
+            next
+          end
+
+          stack << rec.merge(state: state_iter, term: rec[:term])
+        when state_value
+          unless rec[:term].type.dict?
+            yield rec[:path], rec[:term]
+            next
+          end
+
+          stack << rec.merge(state: state_iter, term: rec[:term])
+        else
+          # If state is zero or positive = N, this means nth(N) on the term
+          # and an assertion that term is a dict.
+          dict = rec[:term].unsafe_as_d
+          next unless entry = dict.nth?(rec[:state])
+
+          key, value = entry
+          stack << (rec.merge(state: rec[:state] + 1, term: rec[:term]))
+          stack << (rec.merge(state: state_value, term: value, path: rec[:path].append(value(key))))
+          stack << (rec.merge(state: state_key, term: key, path: rec[:path].append(key(key))))
+        end
+      end
+    end
+
     delegate :unsafe_fetch, :size, to: @steps
 
     {% for method in %w[prepend append] %}
