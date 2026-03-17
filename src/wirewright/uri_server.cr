2148827e29c06a7b912fccf419c211747cbf542f
Alexey Yurchenko <homonoidian@proton.me>
2026-03-18 02:48:53 +0300

    ww: Upload improvements to `URIServer`

--- src/wirewright/uri_server.cr
+++ src/wirewright/uri_server.cr
@@ -7,6 +7,8 @@ module Ww
   module URIServer
     extend self
 
+    Log = ::Log.for(self)
+
     defrecord Present, content : Term::Blob
     defrecord Absent, detail : String
 
@@ -15,6 +17,8 @@ module Ww
     @@r_supply = Pf::Map(URI, Present | Absent).new
     @@r_retrying = Pf::Set(URI).new
     @@r_changed = BlockingSignal.new
+
+    @@r_waiters_signal = BlockingSignal.new
 
     alias Msg = URIAdded
 
@@ -29,6 +33,8 @@ module Ww
     end
 
     def msgloop
+      Log.debug { "msgloop: running" }
+
       epoch = 0u64
 
       loop do
@@ -46,8 +52,15 @@ module Ww
         wg = WaitGroup.new(demand.size)
 
         demand.each do |uri|
-          wg.spawn(name: "fetch worker") do
-            unless fetch?(uri)
+          wg.add(1)
+
+          spawn(name: "fetch worker") do
+            case fetch(uri)
+            in .fetched?
+              wg.done
+            in .retry_later?
+              wg.done
+              # Start retrying on this fiber instead of spawning a new one.
               retry(uri)
             end
           end
@@ -57,38 +70,91 @@ module Ww
       end
     end
 
-    # Returns `true` if *uri* was fetched and `@@r_supply` was updated appropriately.
-    private def fetch?(uri : URI) : Bool
-      begin
-        status = 404
+    enum FetchResult
+      # The initial URL was fetched and `@@r_supply` was updated appropriately.
+      Fetched
 
-        content = Term::Blob.build do |dst|
+      # The initial URL was not fetched. The request should be retried later,
+      # if possible.
+      RetryLater
+    end
+
+    # This should clamp cyclic redirects & general bad behavior.
+    MAX_REQUEST_CHAIN_SIZE = 8
+
+    private def fetch(initial_uri : URI) : FetchResult
+      chain = [initial_uri]
+
+      supply = nil
+      fetch_result = FetchResult::Fetched
+
+      MAX_REQUEST_CHAIN_SIZE.times do
+        break if supply
+
+        uri = chain.last
+
+        begin
+          Log.debug { "fetch(#{initial_uri}): requesting #{uri}" }
+
           HTTP::Client.get(uri, headers: HTTP::Headers{"User-Agent" => "Wirewright"}) do |response|
-            status = response.status
+            Log.debug { "fetch(#{initial_uri}): received response on #{uri} with status: #{response.status}" }
 
-            IO.copy(response.body_io, dst)
+            case response.status
+            when .ok?
+              # HTTP 200: Make a blob from the response body, and we're done.
+              content = Term::Blob.build(classify: false) do |dst|
+                IO.copy(response.body_io, dst)
+              end
+
+              # If the server tells us the MIME type, use that, otherwise, we'll try to
+              # guess it on our end.
+              if mime_type = response.mime_type
+                classif = Term::Blob::Classif.of(mime_type)
+              end
+              content.classify!(classif)
+
+              supply = Present.new(content)
+            when .redirection?
+              # HTTP 300-399: Redirection.
+              #
+              # See https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/Redirections
+              #
+              # Let's assume optimistically that all such requests have a Location. So
+              # retry the request with that.
+              unless location = response.headers["Location"]?
+                supply = Absent.new("HTTP redirection missing Location header")
+                next
+              end
+
+              chain << URI.parse(location)
+            else
+              # Something else. Just give up for now.
+              supply = Absent.new("HTTP status code #{response.status}")
+            end
           end
+        rescue e : IO::Error
+          Log.debug(exception: e) { "fetch(#{initial_uri}): failed to fetch #{uri}" }
+
+          supply = Absent.new("internal error")
+          fetch_result = FetchResult::RetryLater
         end
-
-        if status
-          supply = Present.new(content)
-        else
-          supply = Absent.new("unrecognized HTTP status code #{status}")
-        end
-
-        result = true # fetched
-      rescue e : IO::Error
-        Log.debug(exception: e) { "fetch worker failed for #{uri}" }
-
-        supply = Absent.new("internal error")
-        result = false # did not fetch
       end
+
+      return fetch_result unless supply
+
+      Log.debug { "fetch(#{initial_uri}): completing request chain: #{chain}" }
 
       @@r_lock.synchronize do
-        @@r_supply = @@r_supply.assoc(uri, supply)
+        # We record all URIs that we've fetched (due to redirects there may be many
+        # of them). We'll assoc() the resulting Present/Absent response to *all* of
+        # these URIs (for now; I don't know if that's the expected behavior generally).
+        chain.each do |uri|
+          @@r_supply = @@r_supply.assoc(uri, supply)
+          @@r_waiters_signal.call
+        end
       end
 
-      result
+      fetch_result
     end
 
     # Spawns a fiber to retry *uri* with exponential decay if not already retrying.
@@ -100,35 +166,39 @@ module Ww
         @@r_retrying = @@r_retrying.add(uri)
       end
 
-      spawn(name: "retry fiber for #{uri}") do
-        rng = Random::PCG32.new
+      rng = Random::PCG32.new
 
-        (0..).each do |cycle|
-          case cycle
-          when 0
-            nap = (50..100).sample(rng).milliseconds
-          when 1
-            nap = (100..300).sample(rng).milliseconds
-          when 2
-            nap = (50..500).sample(rng).milliseconds
-          when 3..5
-            k = 2**cycle - 1
-            nap = (k * 50..k * 80).sample(rng).milliseconds
-          else
-            k = 2**6 - 1
-            nap = (k * 50..k * 80).sample(rng).milliseconds
-          end
-
-          Log.trace { "retrying #{uri} after #{nap.humanize}" }
-
-          sleep nap
-
-          break unless @@r_lock.synchronize { uri.in?(@@r_retrying) }
-          break if fetch?(uri)
+      (0..).each do |cycle|
+        case cycle
+        when 0
+          nap = (50..100).sample(rng).milliseconds
+        when 1
+          nap = (100..300).sample(rng).milliseconds
+        when 2
+          nap = (50..500).sample(rng).milliseconds
+        when 3..5
+          k = 2**cycle - 1
+          nap = (k * 50..k * 80).sample(rng).milliseconds
+        else
+          k = 2**6 - 1
+          nap = (k * 50..k * 80).sample(rng).milliseconds
         end
 
-        Log.trace { "complete retrying #{uri}" }
+        Log.trace { "retrying #{uri} after #{nap.humanize}" }
+
+        sleep nap
+
+        break unless @@r_lock.synchronize { uri.in?(@@r_retrying) }
+
+        case fetch(uri)
+        in .fetched?
+          break
+        in .retry_later?
+          # OK, keep trying.
+        end
       end
+
+      Log.trace { "complete retrying #{uri}" }
     end
 
     defrecord Wait
@@ -149,7 +219,7 @@ module Ww
     end
 
     def wait(epoch : UInt64)
-      @@r_changed.wait(epoch)
+      @@r_waiters_signal.wait(epoch)
     end
   end
 end
