dc6b18b778ce7eeb6395389ba20ec73cdbe79e43
Alexey Yurchenko <homonoidian@proton.me>
2026-05-12 03:37:44 +0300

    ww: Broaden report invalidation in `PathService`
    Invalidate subject paths & their parents.

--- src/wirewright/service/path_service.cr
+++ src/wirewright/service/path_service.cr
@@ -210,7 +210,11 @@ module Ww
       end
 
       private def handle(msg : PathMonitorService::Notification) : Nil
+        # For directories.
         PathService.invalidate(msg.path, Report)
+        # For directory entry events.
+        PathService.invalidate(msg.path.parent, Report)
+
         if msg.is_a?(PathMonitorService::FileCommitted)
           PathService.invalidate(msg.path, Reading)
         end
