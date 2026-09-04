5c2b3b391186e7d2cd545aeeffad463d5c6645c4
Alexey Yurchenko <homonoidian@proton.me>
2026-09-04 04:04:01 +0300

    ww: Don't try to fix what's not broken
    This code is nearing its end anyway.

--- src/wirewright/uri_server.cr
+++ src/wirewright/uri_server.cr
@@ -99,10 +99,7 @@ module Ww
         begin
           Log.debug { "fetch(#{initial_uri}): requesting #{uri}" }
 
-          tls_context = OpenSSL::SSL::Context::Client.new
-          tls_context.set_default_verify_paths
-
-          HTTP::Client.get(uri, headers: HTTP::Headers{"User-Agent" => ::Ww::USER_AGENT}, tls: tls_context) do |response|
+          HTTP::Client.get(uri, headers: HTTP::Headers{"User-Agent" => ::Ww::USER_AGENT}) do |response|
             Log.debug { "fetch(#{initial_uri}): received response on #{uri} with status: #{response.status}" }
 
             case response.status
