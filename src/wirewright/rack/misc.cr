23ed713144548b30fb897b1dedcd93437e63651c
Alexey Yurchenko <homonoidian@proton.me>
2026-09-11 03:09:42 +0300

    rack: Split the `propose`/`step` function by node for performance
    This seems to improve the running time of tests somewhat, although maybe
    it's just a measurement error :)

--- src/wirewright/rack/misc.cr
+++ src/wirewright/rack/misc.cr
@@ -10,165 +10,225 @@ module Ww::Rack::Misc
   end
 
   private def propose(hg : D7::Hypergraph, proposals)
-    hg.propose(proposals, :discard, :latest, :feed, :transfer, :delay, :view, :journal) do |candidate|
-      step(hg, candidate)
+    # NOTE: We split the propose() for each node for performance, so that code corresponding
+    # to nodes absent in *hg* is entirely pruned, without imposing even the cost of
+    # a probably_matches call.
+    propose_discard(hg, proposals)
+    propose_latest(hg, proposals)
+    propose_feed(hg, proposals)
+    propose_transfer(hg, proposals)
+    propose_delay(hg, proposals)
+    propose_view(hg, proposals)
+    propose_journal(hg, proposals)
+  end
+
+  private def propose_discard(hg : D7::Hypergraph, proposals) : Nil
+    hg.propose(proposals, :discard) do |node|
+      Term.case(node.term) do
+        # Discard can target zero or more nodes.
+        matchpi %{[discard @edge_]} do
+          targets = Pf::Kit.stack_array(D7::Node, 4)
+
+          Rack.each_cell(hg, hg.resolve(node.addr, edge)) do |target|
+            next if target.empty?
+
+            targets << target.node
+          end
+
+          targets.present? ? D7.patches(targets, {2, nil}) : nil
+        end
+
+        # Discard with a pattern can target zero or more nodes.
+        matchpi %{[discard @edge_ pattern_]} do
+          targets = Pf::Kit.stack_array(D7::Node, 4)
+
+          Rack.each_cell(hg, hg.resolve(node.addr, edge)) do |target|
+            next unless value = target.value?
+            next unless M1.probe?(pattern, value)
+
+            targets << target.node
+          end
+
+          targets.present? ? D7.patches(targets, {2, nil}) : nil
+        end
+
+        otherwise { }
+      end
     end
   end
 
-  private def step(hg : D7::Hypergraph, node : D7::Node) : D7::Patch?
-    Term.case(node.term, block_type: {:proc, hg : D7::Hypergraph, node : D7::Node}) do
-      # Discard can target zero or more nodes.
-      matchpi %{[discard @edge_]} do
-        targets = Pf::Kit.stack_array(D7::Node, 4)
+  private def propose_latest(hg : D7::Hypergraph, proposals) : Nil
+    hg.propose(proposals, :latest) do |node|
+      Term.case(node.term) do
+        matchpi %{[latest @edge_]}, %{[latest @edge_ _]} do
+          next unless cell = Rack.cell?(hg, hg.resolve(node.addr, edge))
 
-        Rack.each_cell(hg, hg.resolve(node.addr, edge)) do |target|
-          next if target.empty?
-
-          targets << target.node
+          D7.patch(node, {2, cell.value?})
         end
 
-        targets.present? ? D7.patches(targets, {2, nil}) : nil
+        otherwise { }
       end
+    end
+  end
 
-      # Discard with a pattern can target zero or more nodes.
-      matchpi %{[discard @edge_ pattern_]} do
-        targets = Pf::Kit.stack_array(D7::Node, 4)
-
-        Rack.each_cell(hg, hg.resolve(node.addr, edge)) do |target|
-          next unless value = target.value?
-          next unless M1.probe?(pattern, value)
-
-          targets << target.node
-        end
-
-        targets.present? ? D7.patches(targets, {2, nil}) : nil
-      end
-
-      matchpi %{[latest @edge_]}, %{[latest @edge_ _]} do
-        return unless cell = Rack.cell?(hg, hg.resolve(node.addr, edge))
-
-        D7.patch(node, {2, cell.value?})
-      end
-
-      matchpi %{[feed _*]} do
-        return unless spec = Feed.spec?(node.term)
+  private def propose_feed(hg : D7::Hypergraph, proposals) : Nil
+    hg.propose(proposals, :feed) do |node|
+      Term.matchpi?(node.term, %{[feed _*]}) do
+        next unless spec = Feed.spec?(node.term)
 
         Feed.step(hg, node, spec)
       end
+    end
+  end
 
-      matchpi %{[delay 1 successor_]} do
-        D7.replace(node, successor)
-      end
-
-      matchpiT %{[delay n←(%number +i32!) _]} do
-        D7.patch(node, {1, n - 1})
-      end
-
-      # This one is used in some mixture()s wher ewe require the circuit to evolve
-      # despite the possibility of no other changes.
-      matchpiT %{[delay n←(%number +i32!)]} do
-        D7.patch(node, {1, n - 1})
-      end
-
-      matchpi %{[transfer (@src_ pattern_ @dst_) template_]} do
-        transfer(hg, node, src, pattern, dst, template)
-      end
-
-      matchpi %{[transfer (srcs←((%past @_ min: 1)) pattern_ @dst_) template_]} do
-        transfer(hg, node, srcs.items, pattern, dst, template)
-      end
-
-      matchpi %{[transfer (not←(not (%past @_ min: 0)) srcs←((%past @_ min: 1)) pattern_ @dst_) template_]} do
-        inhibitorsQ = not.items.move(1)
-        active = inhibitorsQ.all? do |inhibitorQ|
-          inhibitor_cell = Rack.cell?(hg, hg.resolve(node.addr, inhibitorQ))
-          inhibitor_cell.nil? || inhibitor_cell.empty?
+  private def propose_delay(hg : D7::Hypergraph, proposals) : Nil
+    hg.propose(proposals, :delay) do |node|
+      Term.case(node.term) do
+        matchpi %{[delay 1 successor_]} do
+          D7.replace(node, successor)
         end
 
-        next unless active
+        matchpiT %{[delay n←(%number +i32!) _]} do
+          D7.patch(node, {1, n - 1})
+        end
 
-        transfer(hg, node, srcs.items, pattern, dst, template)
+        # This one is used in some mixture()s wher ewe require the circuit to evolve
+        # despite the possibility of no other changes.
+        matchpiT %{[delay n←(%number +i32!)]} do
+          D7.patch(node, {1, n - 1})
+        end
+
+        otherwise { }
       end
+    end
+  end
 
-      # TODO: cache
-      matchpi %{[view (@src_ pattern_ @dst_) template_]} do
-        return unless dst_cell = Rack.cell?(hg, hg.resolve(node.addr, dst))
-
-        src_cell = Rack.cell?(hg, hg.resolve(node.addr, src))
-
-        # If the source cell is missing or empty, clear the destination cell.
-        if src_cell.nil? || (matchee = src_cell.value?).nil?
-          return D7.patch(dst_cell.node, {2, nil})
+  private def propose_transfer(hg : D7::Hypergraph, proposals) : Nil
+    hg.propose(proposals, :transfer) do |node|
+      Term.case(node.term) do
+        matchpi %{[transfer (@src_ pattern_ @dst_) template_]} do
+          transfer(hg, node, src, pattern, dst, template)
         end
 
-        # Clear the destination cell on pattern mismatch.
-        unless env = M1.match?(pattern, matchee)
-          return D7.patch(dst_cell.node, {2, nil})
+        matchpi %{[transfer (srcs←((%past @_ min: 1)) pattern_ @dst_) template_]} do
+          transfer(hg, node, srcs.items, pattern, dst, template)
         end
 
-        expansion = Alloy.render_rep(template, locals: env)
-        unless expansion.empty?
-          output = Term.collapse(expansion)
-        end
-
-        D7.patch(dst_cell.node, {2, output})
-      end
-
-      # TODO: cache
-      matchpi %{[view (srcs←((%past @_ min: 1)) pattern_ @dst_) template_]} do
-        return unless dst_cell = Rack.cell?(hg, hg.resolve(node.addr, dst))
-
-        row = Term::Dict.build do |commit|
-          srcs.items.each do |src|
-            src_cell = Rack.cell?(hg, hg.resolve(node.addr, src))
-
-            # If one of the source cells is missing or empty, clear the destination cell.
-            if src_cell.nil? || (matchee = src_cell.value?).nil?
-              return D7.patch(dst_cell.node, {2, nil})
-            end
-
-            commit << matchee
+        matchpi %{[transfer (not←(not (%past @_ min: 0)) srcs←((%past @_ min: 1)) pattern_ @dst_) template_]} do
+          inhibitorsQ = not.items.move(1)
+          active = inhibitorsQ.all? do |inhibitorQ|
+            inhibitor_cell = Rack.cell?(hg, hg.resolve(node.addr, inhibitorQ))
+            inhibitor_cell.nil? || inhibitor_cell.empty?
           end
+
+          next unless active
+
+          transfer(hg, node, srcs.items, pattern, dst, template)
         end
 
-        # Clear the destination cell on pattern mismatch.
-        unless env = M1.match?(pattern, Term.of(row))
-          return D7.patch(dst_cell.node, {2, nil})
-        end
-
-        expansion = Alloy.render_rep(template, locals: env)
-        unless expansion.empty?
-          output = Term.collapse(expansion)
-        end
-
-        D7.patch(dst_cell.node, {2, output})
+        otherwise { }
       end
+    end
+  end
 
-      matchpi %{[journal (@edge_) _*]} do
-        return unless target = Rack.cell?(hg, hg.resolve(node.addr, edge))
-        return unless goal = target.value?
+  private def propose_view(hg : D7::Hypergraph, proposals) : Nil
+    hg.propose(proposals, :view) do |node|
+      Term.case(node.term) do
+        # TODO: cache
+        matchpi %{[view (@src_ pattern_ @dst_) template_]} do
+          next unless dst_cell = Rack.cell?(hg, hg.resolve(node.addr, dst))
 
-        event = Term.of(:appeared, goal)
-        D7.patch(node, {1, 1, goal}, {node.term.itemsize, event})
-      end
+          src_cell = Rack.cell?(hg, hg.resolve(node.addr, src))
 
-      matchpi %{[journal (@edge_ state_) _*]} do
-        return unless target = Rack.cell?(hg, hg.resolve(node.addr, edge))
+          # If the source cell is missing or empty, clear the destination cell.
+          if src_cell.nil? || (matchee = src_cell.value?).nil?
+            next D7.patch(dst_cell.node, {2, nil})
+          end
 
-        # State present, goal absent, we need to remove state.
-        unless goal = target.value?
-          event = Term.of(:disappeared, state)
-          return D7.patch(node, {1, 1, nil}, {node.term.itemsize, event})
+          # Clear the destination cell on pattern mismatch.
+          unless env = M1.match?(pattern, matchee)
+            next D7.patch(dst_cell.node, {2, nil})
+          end
+
+          expansion = Alloy.render_rep(template, locals: env)
+          unless expansion.empty?
+            output = Term.collapse(expansion)
+          end
+
+          D7.patch(dst_cell.node, {2, output})
         end
 
-        return if state == goal
+        # TODO: cache
+        matchpi %{[view (srcs←((%past @_ min: 1)) pattern_ @dst_) template_]} do
+          next unless dst_cell = Rack.cell?(hg, hg.resolve(node.addr, dst))
 
-        # State present, goal changed.
-        event = Term.of(:changed, goal)
-        D7.patch(node, {1, 1, nil}, {node.term.itemsize, event})
+          valid = true
+
+          row = Term::Dict.build do |commit|
+            srcs.items.each do |src|
+              src_cell = Rack.cell?(hg, hg.resolve(node.addr, src))
+
+              # If one of the source cells is missing or empty, clear the destination cell.
+              if src_cell.nil? || (matchee = src_cell.value?).nil?
+                valid = false
+                break
+              end
+
+              commit << matchee
+            end
+          end
+
+          unless valid
+            next D7.patch(dst_cell.node, {2, nil})
+          end
+
+          # Clear the destination cell on pattern mismatch.
+          unless env = M1.match?(pattern, Term.of(row))
+            next D7.patch(dst_cell.node, {2, nil})
+          end
+
+          expansion = Alloy.render_rep(template, locals: env)
+          unless expansion.empty?
+            output = Term.collapse(expansion)
+          end
+
+          D7.patch(dst_cell.node, {2, output})
+        end
+
+        otherwise { }
       end
+    end
+  end
 
-      otherwise { }
+  private def propose_journal(hg : D7::Hypergraph, proposals) : Nil
+    hg.propose(proposals, :journal) do |node|
+      Term.case(node.term) do
+        matchpi %{[journal (@edge_) _*]} do
+          next unless target = Rack.cell?(hg, hg.resolve(node.addr, edge))
+          next unless goal = target.value?
+
+          event = Term.of(:appeared, goal)
+          D7.patch(node, {1, 1, goal}, {node.term.itemsize, event})
+        end
+
+        matchpi %{[journal (@edge_ state_) _*]} do
+          next unless target = Rack.cell?(hg, hg.resolve(node.addr, edge))
+
+          # State present, goal absent, we need to remove state.
+          unless goal = target.value?
+            event = Term.of(:disappeared, state)
+            next D7.patch(node, {1, 1, nil}, {node.term.itemsize, event})
+          end
+
+          next if state == goal
+
+          # State present, goal changed.
+          event = Term.of(:changed, goal)
+          D7.patch(node, {1, 1, nil}, {node.term.itemsize, event})
+        end
+
+        otherwise { }
+      end
     end
   end
 
